35008288f2675f72dd21982b42431ee55cc21da0
HEBER-PC\heber <heber.navegante@gmail.com>
2018-05-02 00:36:41 -0300

    tornando funções privadas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,176 +1,178 @@
-$(function () {
-  topMenuEvents();
+(function () {
+  $(function () {
+    topMenuEvents();
 
-  initSideMenu();
+    initSideMenu();
 
-  setProductsWidth();
+    setProductsWidth();
 
-  initCarousel();
+    initCarousel();
 
-  invertSideMenuAndCarousel();
-});
+    invertSideMenuAndCarousel();
+  });
 
-function setProductsWidth() {
-  if(isMobile()) {
-    var $products = $(".container-carousel .product");
+  function setProductsWidth() {
+    if (isMobile()) {
+      var $products = $(".container-carousel .product");
 
-    for (let i = 0; i < $products.length; i++) {
-      var $product = $($products[i]);
+      for (let i = 0; i < $products.length; i++) {
+        var $product = $($products[i]);
 
-      $product.attr('style', 'width: ' +  $(window).width() + "px !important");
+        $product.attr('style', 'width: ' + $(window).width() + "px !important");
+      }
     }
   }
-}
 
-function invertSideMenuAndCarousel() {
-  if(isMobile()) {
-    
-    var $producsContainer = $(".products-container");
+  function invertSideMenuAndCarousel() {
+    if (isMobile()) {
 
-    var $sideMenu = $(".products-container .side-menu");
+      var $producsContainer = $(".products-container");
 
-    $producsContainer.append($sideMenu)  ;
+      var $sideMenu = $(".products-container .side-menu");
+
+      $producsContainer.append($sideMenu);
+    }
   }
-}
 
-function isMobile() {
-  var check = false;
-  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-  return check;
-}
+  function isMobile() {
+    var check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+  }
 
-function topMenuEvents() {
-  if (isMobile()) {
+  function topMenuEvents() {
+    if (isMobile()) {
 
-    var $expand = $("#expand-menu");
+      var $expand = $("#expand-menu");
 
-    $expand.show();
-
-    var $compress = $("#compress-menu");
-
-    var $topMenuContent = $(".top-menu .content");
-
-    $expand.click(function () {
-      $expand.hide();
-      $compress.show();
-
-      $topMenuContent.show();
-    });
-
-    $compress.click(function () {
-      $compress.hide();
       $expand.show();
 
-      $topMenuContent.hide();
+      var $compress = $("#compress-menu");
+
+      var $topMenuContent = $(".top-menu .content");
+
+      $expand.click(function () {
+        $expand.hide();
+        $compress.show();
+
+        $topMenuContent.show();
+      });
+
+      $compress.click(function () {
+        $compress.hide();
+        $expand.show();
+
+        $topMenuContent.hide();
+      });
+    }
+  }
+
+  function initSideMenu() {
+    getSideBarJson()
+      .done(function (response) {
+        var template = sideBarTemplate(response);
+
+        $('.side-menu').append(template);
+      })
+  }
+
+  function sideBarTemplate(items) {
+    var result = '';
+
+    for (let i = 0; i < items.length; i++) {
+      var item = items[i];
+
+      result +=
+        '<div class="item">' +
+        '<div class="category">' + (item.action ? '<a href="' + item.action + '">' + item.category + '</a>' : item.category) + '</div>' +
+        menusTemplate(item.menus) +
+        '</div>';
+    }
+
+    return result;
+  }
+
+  function menusTemplate(items) {
+    var result = '';
+
+    for (let i = 0; i < items.length; i++) {
+      var item = items[i];
+
+      result +=
+        '<div class="menus">' +
+        '<div class="name">' + item.name + '</div>' +
+        menuItems(item.itens) +
+        '</div>';
+    }
+
+    return result;
+  }
+
+  function menuItems(items) {
+    var result = '';
+
+    for (let i = 0; i < items.length; i++) {
+      var item = items[i];
+
+      result += '<a href="' + item.action + '"><div class="menu-item">' + item.name + ' (' + item.count + ')</div></a>';
+    }
+
+    return result;
+  }
+
+  function getSideBarJson() {
+    return $.ajax({
+      url: "/side_bar.json",
+      method: "GET",
+      header: { "Accept": "application/json" }
     });
   }
-}
 
-function initSideMenu() {
-  getSideBarJson()
-    .done(function (response) {
-      var template = sideBarTemplate(response);
+  function initCarousel() {
+    var $carousel = $(".owl-carousel");
+    $carousel.show();
 
-      $('.side-menu').append(template);
+    setDots($carousel.find(".description .text"));
+
+    var $images = $carousel.find(".product .description .image");
+
+    for (let i = 0; i < $images.length; i++) {
+      var $element = $($images[i]);
+
+      $element.css({
+        "background-image": "url(assets/images/tv.jpg)"
+      });
+    }
+
+    var checkifIsMobile = isMobile();
+
+    $carousel = $carousel.owlCarousel({
+      items: 3,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      margin: checkifIsMobile ? 0 : 12,
+      autoWidth: true,
+      center: checkifIsMobile
+    });
+
+    setEvents($carousel);
+  }
+
+  function setDots($element) {
+    $element.dotdotdot({
+      ellipsis: "\u2026 "
     })
-}
-
-function sideBarTemplate(items) {
-  var result = '';
-
-  for (let i = 0; i < items.length; i++) {
-    var item = items[i];
-
-    result +=
-      '<div class="item">' +
-      '<div class="category">'+ (item.action ? '<a href="'+ item.action +'">'+ item.category +'</a>' : item.category) + '</div>' +
-      menusTemplate(item.menus) +
-      '</div>';
   }
 
-  return result;
-}
+  function setEvents($owl) {
+    $("#arrow-prev").click(function () {
+      $owl.trigger('prev.owl.carousel');
+    });
 
-function menusTemplate(items) {
-  var result = '';
-
-  for (let i = 0; i < items.length; i++) {
-    var item = items[i];
-
-    result +=
-      '<div class="menus">' +
-      '<div class="name">' + item.name + '</div>' +
-      menuItems(item.itens) +
-      '</div>';
-  }
-
-  return result;
-}
-
-function menuItems(items) {
-  var result = '';
-
-  for (let i = 0; i < items.length; i++) {
-    var item = items[i];
-
-    result += '<a href="' + item.action + '"><div class="menu-item">' + item.name + ' (' + item.count + ')</div></a>';
-  }
-
-  return result;
-}
-
-function getSideBarJson() {
-  return $.ajax({
-    url: "/side_bar.json",
-    method: "GET",
-    header: { "Accept": "application/json" }
-  });
-}
-
-function initCarousel() {
-  var $carousel = $(".owl-carousel");
-  $carousel.show();
-
-  setDots($carousel.find(".description .text"));
-
-  var $images = $carousel.find(".product .description .image");
-
-  for (let i = 0; i < $images.length; i++) {
-    var $element = $($images[i]);
-
-    $element.css({
-      "background-image": "url(assets/images/tv.jpg)"
+    $("#arrow-next").click(function () {
+      $owl.trigger('next.owl.carousel');
     });
   }
-
-  var checkifIsMobile = isMobile();
-
-  $carousel = $carousel.owlCarousel({
-    items: 3,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: true,
-    margin: checkifIsMobile ? 0 : 12,
-    autoWidth: true,
-    center: checkifIsMobile
-  });
-
-  setEvents($carousel);
-}
-
-function setDots($element) {
-  $element.dotdotdot({
-    ellipsis: "\u2026 "
-  })
-}
-
-function setEvents($owl) {
-  $("#arrow-prev").click(function () {
-    $owl.trigger('prev.owl.carousel');
-  });
-
-  $("#arrow-next").click(function () {
-    $owl.trigger('next.owl.carousel');
-  });
-}
+})();
